b3bd1d1b41eba4e051583fd70adb91d40493358d
Patryk <patryk.nietyksza@gmail.com>
2025-05-11 17:19:49 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use colored::{self, Colorize};
+use rand::random_range;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    print!("New Project");
+    println!("Guest the number.");
+
+    let secret_number = random_range(1..=100);
+
+    loop {
+        println!("Enter your number:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Cannot read the line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Please enter your number.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("{}", "Bravo, you've got it!".on_green());
+                break;
+            }
+        }
+    }
 }
